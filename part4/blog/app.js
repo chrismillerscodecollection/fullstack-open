973fd2805fdf6163b4f67588b03ffbd85bdefd0a
Christopher Miller <chris0007@gmail.com>
2025-12-27 12:57:43 -0600

    feat: refactor MongoDB connection handling, update environment variable logging, and enhance test setup

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -9,14 +9,17 @@ const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(config.MONGODB_URI)
+    logger.info(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+connectDB()
 
 app.use(express.json())
 app.use(middleware.requestLogger)
--- part4/blog/test/blog_supertests.js
+++ part4/blog/test/blog_supertests.js
@@ -1,12 +1,46 @@
+const request = require('supertest')
 const { test } = require('node:test')
-const supertest = require('supertest')
-const app = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
+const middleware = require('../utils/middleware')
+const blogRouter = require('../controllers/blogs')
 
+logger.info(`Connecting to: ${config.MONGODB_URI}`)
 
-test('blogs are returned', () => {
-    api
-        .get('/')
-        .expect(200)
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect('Content-Length', '500')
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(config.MONGODB_URI)
+    logger.info(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+const app = express()
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+test.before(async () => {
+  await connectDB()
+})
+
+test.after(async () => {
+  await mongoose.connection.close()
+})
+
+test('returns all blogs', () => {
+  return request(app)
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .then(res => {
+      console.log(res.body)
+    })
 })
