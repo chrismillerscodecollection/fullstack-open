abbe66068afe286566710f735c096c0c55176641
Christopher Miller <chris0007@gmail.com>
2025-12-29 08:50:24 -0600

    feat: add test for adding new blog to mongoDB

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,7 +2,7 @@ const { app, connectDB } = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-await connectDB()
+connectDB()
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
--- part4/blog/test/blog_supertests.js
+++ part4/blog/test/blog_supertests.js
@@ -13,7 +13,47 @@ after(async () => {
   await mongoose.connection.close()
 })
 
-test('GET /api/blog returns 200', async () => {
-  const response = await request(app).get('/api/blogs')
-  assert.strictEqual(response.status, 200)
+describe('get requests to mongoDB', () => {
+  test('get /api/blogs returns 200 + first blog', async () => {
+    const response = await request(app)
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blog = {
+      title: 'Building Scalable Microservices with Node.js',
+      author: 'Sarah Mitchell',
+      url: 'https://devblog.tech/microservices-nodejs',
+      likes: 42,
+      id: '6951c854bc953a5ef435215a'
+    }
+
+    assert.deepStrictEqual(response.body[0], blog)
+  })
+})
+
+describe('post requests to mongoDB', () => {
+  test('post /api/blogs returns 201 + adds new blog post', async () => {
+    const response = await request(app)
+      .post('/api/blogs')
+      .send({
+        title: 'Understanding React Hooks in Depth',
+        author: 'James Anderson',
+        url: 'https://frontend-masters.io/react-hooks-guide',
+        likes: 128,
+      })
+      .expect(201)
+
+    assert.ok(response.body.id)
+  })
+
+  test('handles missing property - "likes"', async () => {
+    const response = await request(app)
+      .post('/api/blogs')
+      /send({
+        title: ''
+      })
+  })
+
+
 })
