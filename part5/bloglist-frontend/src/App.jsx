18eb7472022c86a46114dccec01f94240cc163ef
Christopher Miller <chris0007@gmail.com>
2026-01-05 06:40:27 -0600

    Persist login after refresh Add logout functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -27,17 +27,47 @@ const App = () => {
     getBlogs()
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
 
     try {
       console.log('Logging in user:', username)
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
+      console.log('Logging in user:', user)
       setUsername('')
       setPassword('')
     } catch {
       setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async () => {
+    try {
+      window.localStorage.removeItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      setUser('')
+      setUsername('')
+      setPassword('')
+    } catch(error) {
+      setErrorMessage(`Unable to logout the user due to ${error}`)
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -70,6 +100,12 @@ const App = () => {
     </form>
   )
 
+  const logoutForm = () => (
+    <form onSubmit={handleLogout}>
+      <button type="submit">logout</button>
+    </form>
+  )
+
   if (user === null) {
     return (
       <div>
@@ -83,10 +119,9 @@ const App = () => {
   return (
     <div>
       <p>{user.name} logged in</p>
-
+      {logoutForm()}
       <h2>blogs</h2>
       <Notification message={errorMessage} />
-
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
